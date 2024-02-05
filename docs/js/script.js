eebb75dc0fa019fbd36dec9ec353b3db59e94c34
Jop <jopmolenaar@icloud.com>
2024-02-05 15:05:17 +0100

    added js code to get data from json

--- docs/js/script.js
+++ docs/js/script.js
@@ -1,1 +1,9 @@
 console.log("hi");
+
+fetch("../../reizen.json")
+    .then((response) => response.json())
+    .then((data) => showInfo(data));
+
+function showInfo(data) {
+    console.table(data);
+}
