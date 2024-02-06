136b90b44e03aaccaf618281d201b7b16161bc66
Jop <jopmolenaar@icloud.com>
2024-02-06 13:52:32 +0100

    content rendered for second section

--- docs/js/script.js
+++ docs/js/script.js
@@ -1,16 +1,22 @@
-/*
- *
- *
- *
- *
- **/
+/**
+ * @Author:            Jop Molenaar
+ * @Date created:       05-02-2024
+ * @Date updated:       06-02-2024
+ * @Description:        Store a new device in the database
+ */
 
 // Fetch the json file
 fetch("../../reizen.json")
     .then((response) => response.json())
-    .then((data) => createCards(data));
+    .then((data) => {
+        createCards(data);
+        addContentToBucketListCountry(data);
+    });
 
-//  Create four cards
+/**
+ * Create four cards
+ * @param {Object} data - The data out of the fetched url
+ */
 const createCards = (data) => {
     console.log(data);
     const countryCards = data.visitedCountries.slice(0, 4); // Limit to the first four elements
@@ -26,4 +32,17 @@ const createCards = (data) => {
         li.append(img);
         cardList.append(li);
     });
+};
+
+/**
+ * Add content to the second section of the page
+ * @param {Object} data - The data out of the fetched url
+ */
+const addContentToBucketListCountry = (data) => {
+    const bucketListTitle = document.querySelector("#bucketListCountry");
+    const reason = document.querySelector("#reason");
+    const bucketListCountry = data.bucketList.slice(0, 1);
+    bucketListTitle.textContent =
+        "I still want to go to " + bucketListCountry[0].country;
+    reason.textContent = bucketListCountry[0].reason;
 };
