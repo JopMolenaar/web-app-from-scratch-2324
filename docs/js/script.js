6f98decea31b6cd79fcd56540308c1fbca94f212
Jop <jopmolenaar@icloud.com>
2024-02-06 11:25:33 +0100

    added first card data in card place html

--- docs/js/script.js
+++ docs/js/script.js
@@ -1,9 +1,24 @@
-console.log("hi");
+/*
+ *
+ *
+ *
+ *
+ **/
 
+// Fetch the json file
 fetch("../../reizen.json")
     .then((response) => response.json())
-    .then((data) => showInfo(data));
+    .then((data) => createCards(data));
 
-function showInfo(data) {
-    console.table(data);
-}
+//  Create four cards
+const createCards = (data) => {
+    console.log(data);
+    const countryCards = data.visitedCountries.slice(0, 4); // Limit to the first four elements
+    const cardList = document.querySelector("#cardList");
+
+    countryCards.forEach((country) => {
+        const li = document.createElement("li");
+        li.textContent = country.country;
+        cardList.append(li);
+    });
+};
