d0e5f7685c99e998f27d81a15f495af1aa45279e
Jop <jopmolenaar@icloud.com>
2024-02-07 14:59:04 +0100

    added smooth scrolling animation for the header

--- docs/js/scrollAnimation.js
+++ docs/js/scrollAnimation.js
@@ -1,24 +1,26 @@
-window.addEventListener("scroll", function () {
-    console.log("ewojnfejwnjfk");
-    const element = document.querySelector(
-        "main div > section:nth-child(2) img:nth-child(5)"
+const div = document.querySelector("main > div");
+
+let visible;
+const element = document.querySelector(
+    "main div > section:nth-child(2) img:nth-child(4)"
+);
+
+const isInViewport = function (ele) {
+    const rect = ele.getBoundingClientRect();
+    return (
+        rect.left >= 0 &&
+        rect.bottom <=
+            (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <=
+            (window.innerWidth || document.documentElement.clientWidth)
     );
-    let scrollPosition =
-        window.pageYOffset || document.documentElement.scrollTop;
-    let viewportHeight = window.innerHeight;
+};
 
-    // Calculate the distance between the top of the element and the bottom of the viewport
-    let elementOffsetTop = element.offsetTop;
-    let distanceFromViewport =
-        elementOffsetTop - scrollPosition - viewportHeight;
-
-    // Check if the element is within the viewport when scrolling
-    console.log(distanceFromViewport);
-    if (distanceFromViewport < 0) {
-        // element.style.display = "block";
-        console.log("in");
+div.addEventListener("scroll", function () {
+    visible = isInViewport(element);
+    if (visible === true) {
+        element.classList.add("inViewPort");
     } else {
-        console.log("out");
-        // element.style.display = "none";
+        element.classList.remove("inViewPort");
     }
 });
